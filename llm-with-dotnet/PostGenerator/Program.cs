0c80f2457ff716a348121564bf30dc7edbb91e42
Maho Pacheco <mapach@microsoft.com>
2024-02-27 15:50:34 -0800

    First post

---
+++ llm-with-dotnet/PostGenerator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.CommandLine.Parsing;
+using System.CommandLine;
+using System;
+
+var personaOption = new Option<string>("--persona")
+{
+    Description = "The persona to use",
+    IsRequired = true
+};
+
+var topicOption = new Option<string>("--topic")
+{
+    Description = "The topic to use",
+    IsRequired = true
+};
+
+var styleOption = new Option<string>("--style")
+{
+    Description = "The style to use",
+    IsRequired = true
+};
+
+var rootCommand = new RootCommand();
+
+var createPostCommand = new Command("create-post", "Create new post");
+
+createPostCommand.AddOption(personaOption);
+createPostCommand.AddOption(topicOption);
+createPostCommand.AddOption(styleOption);
+
+createPostCommand.SetHandler((persona, topic, option) =>
+{
+    Console.WriteLine($"Creating new post with persona: {persona}, topic: {topic}, style: {option}");
+
+    throw new NotImplementedException();
+}, personaOption, topicOption, styleOption);
+
+rootCommand.AddCommand(createPostCommand);
+
+var result = await rootCommand.InvokeAsync(args);
+
+return result;
